93d4e2adb370ee30bfc9e044eb09644bc1fdd27a
cameronotoole44 <cameronotoole404@gmail.com>
2026-02-24 00:43:16 -0500

    now uses data from learned log to add into and tailor the moments more

--- core/moments.py
+++ core/moments.py
@@ -19,54 +19,86 @@ class MomentConfig:
 
 MOMENT_CONFIGS: dict[MomentType, MomentConfig] = {
     MomentType.HYPE: MomentConfig(
-        threshold=3,
+        threshold=2,
         keywords={
+            # classic hype
             'pog', 'poggers', 'pogu', 'pogchamp', 'lets go', 'letsgoo', 'letsgo',
             'gg', 'ez', 'win', 'won', 'clutch', 'insane', 'crazy', 'godlike',
-            'sheesh', 'goated', 'w', 'dub', 'massive', 'huge'
+            'sheesh', 'goated', 'dub', 'massive', 'huge',
+            # from data - CamelCase
+            'pagbounce', 'pagchomp', 'pagman', 'verypog', 'pausershype', 'twitchconhype',
+            'feelsstrongman', 'feelsamazingman', 'imhim', 'bouncinonit', 'robloxcheer',
+            'griddygoose', 'fallwinning',
+            # from data - ALL CAPS
+            'sick', 'wicked', 'holy', 'king', 'letsgo', 'lets',
+            'noway', 'aintnoway', 'noted',
         },
-        fallback_responses=['POGGIES', 'lets gooo', 'HYPERS', 'actual god', 'WE WIN']
+        fallback_responses=['LETSGO', 'SICK', 'PogChamp', 'KING', 'WICKED', 'PagBounce']
     ),
     MomentType.SADGE: MomentConfig(
-        threshold=3,
+        threshold=2,
         keywords={
-            'rip', 'sadge', 'sadchamp', 'f', 'died', 'dead', 'unlucky', 'pain',
-            'pepehands', 'widepeeposad', 'lost', 'lose', 'choke', 'threw', 'ff'
+            # classic sad
+            'rip', 'sadge', 'sadchamp', 'died', 'dead', 'unlucky', 'pain',
+            'pepehands', 'widepeeposad', 'lost', 'lose', 'choke', 'threw', 'ff',
+            # from data - CamelCase
+            'reefersad', 'bigsad', 'porosad', 'notlikethis', 'reallymad', 'ultramad',
+            'wutface', 'donowall', 'pleasestreamer', 'needheal',
+            # from data - ALL CAPS
+            'icant', 'noooo', 'noooyoucant', 'ewww',
         },
-        fallback_responses=['Sadge', 'pain', 'F', 'not like this', 'unlucky']
+        fallback_responses=['Sadge', 'pain', 'NotLikeThis', 'ICANT', 'ReeferSad', 'BigSad']
     ),
     MomentType.GREETING: MomentConfig(
-        threshold=3,
+        threshold=2,
         keywords={
             'hi', 'hello', 'hey', 'sup', 'yo', 'hii', 'hewwo', 'heyy',
-            'whats up', 'wassup', 'waddup', 'greetings'
+            'whats up', 'wassup', 'waddup', 'greetings',
+            # from data
+            'suphomie', 'heyguys', 'raiders',
         },
-        fallback_responses=['hey chat', 'yo', 'hi hi', 'o/']
+        fallback_responses=['hey chat', 'yo', 'SupHomie', 'HeyGuys']
     ),
     MomentType.QUESTION: MomentConfig(
-        threshold=4,
+        threshold=3,
         keywords=set(),
         fallback_responses=['good question', 'true', 'hmm']
     ),
     MomentType.EMOTE_SPAM: MomentConfig(
-        threshold=5,
+        threshold=4,
         keywords=set(),
-        fallback_responses=['KEKW', 'LULW', 'peepoHappy', 'catJAM']
+        fallback_responses=['KEKW', 'LUL', 'PepeLaugh', 'DinkDonk', 'BaNGER', 'LULW']
     ),
 }
 
 COMMON_EMOTES = {
-    'kekw', 'lulw', 'lul', 'omegalul', 'pepega', 'monkas', 'pogchamp', 'pogu',
+    # classic bttv/ffz/7tv
+    'kekw', 'lulw', 'lul', 'lule', 'omegalul', 'pepega', 'monkas', 'pogchamp', 'pogu',
     'poggers', 'pepelaugh', 'catjam', 'peepo', 'peepohappy', 'widepeeposad',
     'sadge', 'copium', 'hopium', 'clueless', 'aware', 'gigachad', 'based',
     'modcheck', 'pausechamp', 'pepejam', 'peped', 'feelsstrongman', 'feelsbadman',
     'feelsgoodman', 'trihard', 'kreygasm', 'residentsleeper', 'notlikethis',
-    'biblethump', 'dansgame', 'wutface', 'jebaited', 'hypers', 'pogo', '4head'
+    'biblethump', 'dansgame', 'wutface', 'jebaited', 'hypers', 'pogo', '4head',
+    # from data - high frequency CamelCase
+    'pauseman', 'banger', 'dinkdonk', 'ravetime', 'wideravetime', 'suphomie',
+    'reefersad', 'pagbounce', 'widehardo', 'weirddude', 'pausershype', 'pagchomp',
+    'imhim', 'cokeshakey', 'speedlaugh', 'puzzletime', 'ashtwerk', 'socute',
+    'ofcourse', 'kappaross', 'giannislaugh', 'feelsdankman', 'dinodance',
+    'robloxcheer', 'singsmic', 'seemsgood', 'thevoices', 'verypog', 'soyr',
+    'pepepls', 'jamfrog', 'bigsad', 'tylerlaugh', 'permavibing', 'pausemansit',
+    'pagman', 'feelsrainman', 'lebron', 'feelsamazingman', 'vislaud', 'pepog',
+    'pewpewpew', 'heyguys', 'griddygoose', 'donowall', 'awkwardsmile', 'aliendance',
+    'bouncinonit', 'cannonboltpls', 'quarterjade', 'reallymad', 'reallynow',
+    # from data - ALL CAPS
+    'lol', 'lmao', 'omg', 'oooo', 'aaaa', 'monka', 'nodders',
+    'saj', 'muga', 'jarvis', 'frog', 'mytrueform', 'schizo', 'wajaja',
+    'icant', 'noooyoucant', 'aintnoway', 'noway', 'vibe', 'gulp', 'crab',
+    'omegadancebutfast', 'kooby',
 }
 
 
 class MomentDetector:
-    def __init__(self, window_size: int = 15):
+    def __init__(self, window_size: int = 20):
         self.window_size = window_size
         self.context_window: list[str] = []
     
@@ -79,7 +111,7 @@ class MomentDetector:
         self.context_window.clear()
     
     def detect(self) -> MomentType:
-        if len(self.context_window) < 5:
+        if len(self.context_window) < 3:
             return MomentType.NEUTRAL
         
         recent = self.context_window[-10:]
@@ -119,10 +151,17 @@ class MomentDetector:
             return True
         if re.match(r'^[A-Z][a-z]+[A-Z]', token):
             return True
+        # common 7TV prefixes
+        lower = token.lower()
+        if lower.startswith(('pepe', 'peepo', 'feels', 'wide', 'cat', 'pog', 'pause')):
+            return True
+        # common 7TV suffixes
+        if lower.endswith(('ge', 'gers', 'jam', 'time', 'dance', 'hype', 'man', 'sad')):
+            return True
         return False
     
     def _contains_keyword(self, message: str, keywords: set[str]) -> bool:
-        tokens = set(re.findall(r'\b\w+\b', message))
+        tokens = set(re.findall(r'\b\w+\b', message.lower()))
         if tokens & keywords:
             return True
         for kw in keywords:
@@ -135,7 +174,7 @@ class MomentDetector:
         config = MOMENT_CONFIGS.get(moment)
         if config and config.fallback_responses:
             return random.choice(config.fallback_responses)
-        return random.choice(['true', 'KEKW', 'peepoHappy', '<3'])
+        return random.choice(['true', 'KEKW', 'PepeLaugh', '<3'])
     
     def get_moment_keywords(self, moment: MomentType) -> set[str]:
         config = MOMENT_CONFIGS.get(moment)
